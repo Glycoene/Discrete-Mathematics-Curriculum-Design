e9f7d5c9fd2e4cb29d04f6d1653e6293d9b00c50
Glycoene <qty2823650328@163.com>
2025-04-19 18:38:31 +0800

    v1.2

--- utils/relational_matrix.py
+++ utils/relational_matrix.py
@@ -1,20 +1,24 @@
+import numpy
+
 def is_symmetric(matrix):
-    length = len(matrix)
-    for i in range(length):
-        for j in range(i + 1, length):
+    rows, cols = matrix.shape
+    if rows != cols:
+        return False
+    for i in range(rows):
+        for j in range(i + 1, cols):
             if matrix[i][j] != matrix[j][i]:
                 return False
     return True
 
 if __name__ == '__main__':
-    matrix1 = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
-    ]
-    matrix2 = [
-        [1, 2, 3],
-        [2, 1, 4],
-        [3, 4, 1]
-    ]
+    matrix1 = numpy.array([
+        [1, 0, 0],
+        [1, 1, 1],
+        [1, 0, 0]
+    ])
+    matrix2 = numpy.array([
+        [1, 1, 0],
+        [1, 1, 1],
+        [0, 1, 1]
+    ])
     print(is_symmetric(matrix1),is_symmetric(matrix2))
